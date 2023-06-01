8f2ab2b58828772415c4d00eb20c8a438611c1af
Mikail Asada <mikailasada@gmail.com>
2023-06-01 20:48:31 +0700

    feat: Activity route

--- src/routes/PrimaryHandler.route.js
+++ src/routes/PrimaryHandler.route.js
@@ -1,12 +1,14 @@
 import AuthRoute from './primary/Auth.route.js';
 import ProfileRoute from './primary/Profile.route.js';
 import SettingsRoute from './primary/Settings.route.js';
+import ActivityRoute from './primary/Activity.route.js';
 
 class PrimaryHandler {
   constructor(server) {
     new AuthRoute(server);
     new ProfileRoute(server);
     new SettingsRoute(server);
+    new ActivityRoute(server);
   }
 }
 
