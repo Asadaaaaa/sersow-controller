5040f7b1a3f0812d0336132bde8b52b1a536533e
Mikail Asada <mikailasada@gmail.com>
2023-06-08 17:24:41 +0700

    fix: file length condition for image3 annd image2

--- src/services/primary/Project.service.js
+++ src/services/primary/Project.service.js
@@ -116,7 +116,7 @@ class ProjectService {
       if (!fileType) return -10;
 
       if (!(fileType && (fileType.ext === 'jpeg' || fileType.ext === 'jpg' || fileType.ext === 'png'))) return -10;
-      if (file.byteLength > 1048576) return -11;
+      if (image2File.byteLength > 1048576) return -11;
       image2Ext = fileType.ext;
     }
 
@@ -128,7 +128,7 @@ class ProjectService {
       if (!fileType) return -12;
 
       if (!(fileType && (fileType.ext === 'jpeg' || fileType.ext === 'jpg' || fileType.ext === 'png'))) return -12;
-      if (image2File.byteLength > 1048576) return -13;
+      if (image3File.byteLength > 1048576) return -13;
       image3Ext = fileType.ext;
     }
 
